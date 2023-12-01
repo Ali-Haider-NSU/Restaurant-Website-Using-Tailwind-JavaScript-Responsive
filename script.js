270070bb79125caa0f89f899525b7a7e65c41d0d
Ali-Haider-NSU <mdalihaider800@gmail.com>
2023-12-01 11:32:55 +0600

    half complete of navigation part

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-let h1 = document.querySelector('h1');
+// To color Nav link Gallery after hovering
+
+let gallery = document.getElementById('gallery');
+let gallink = document.querySelectorAll('.gallink');
+
+gallery.addEventListener('mouseover', () => {
+    gallink[0].style.color = '#FFB03B'
+    gallink[1].style.color = '#FFB03B'
+})
+gallery.addEventListener('mouseout', () => {
+    gallink[0].style.color = ''
+    gallink[1].style.color = ''
+})
