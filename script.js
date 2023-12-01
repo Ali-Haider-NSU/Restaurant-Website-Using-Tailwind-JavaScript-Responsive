fb283c984e492d9526a5750a8d73401a52b2dcad
Ali-Haider-NSU <mdalihaider800@gmail.com>
2023-12-01 21:16:02 +0600

    completion of navigation part

--- script.js
+++ script.js
@@ -1,13 +1,69 @@
-// To color Nav link Gallery after hovering
+// Navigation Part Code
 
 let gallery = document.getElementById('gallery');
 let gallink = document.querySelectorAll('.gallink');
+let subul = document.getElementById('sub-ul');
+let menubar = document.getElementById('menubar');
+let crossbar = document.getElementById('crossbar');
+let mainul = document.getElementById('mainul');
+let angleup = document.getElementById('angleup');
+let angledown = document.getElementById('angledown');
+
+// Hover on Gallery in Large screen
 
 gallery.addEventListener('mouseover', () => {
     gallink[0].style.color = '#FFB03B'
     gallink[1].style.color = '#FFB03B'
+    if (window.innerWidth >= 1000) {
+        subul.style.display = 'block';
+    }
 })
 gallery.addEventListener('mouseout', () => {
     gallink[0].style.color = ''
     gallink[1].style.color = ''
+    if (window.innerWidth >= 1000) {
+        subul.style.display = 'none';
+    }
+})
+
+// Showing Navigation Link when Large Screen
+
+function handleResize() {
+    if (window.innerWidth >= 1000) {
+        mainul.style.display = 'block'
+        crossbar.style.display = 'block'
+        menubar.style.display = 'none'
+    } else {
+        mainul.style.display = 'none'
+        menubar.style.display = 'block'
+        crossbar.style.display = 'none'
+    }
+}
+window.addEventListener("resize", handleResize);
+handleResize();
+
+// Clicking Hamburger/Menubar to Block UL when small screen
+
+menubar.addEventListener('click', () => {
+    mainul.style.display = 'block'
+    menubar.style.display = 'none'
+    crossbar.style.display = 'block'
+})
+crossbar.addEventListener('click', () => {
+    mainul.style.display = 'none'
+    menubar.style.display = 'block'
+    crossbar.style.display = 'none'
+})
+
+// Showing Sub Ul inside the Gallery by clicking angleup and angledown
+
+angledown.addEventListener('click', () => {
+    subul.style.display = 'block'
+    angleup.style.display = 'block'
+    angledown.style.display = 'none'
+})
+angleup.addEventListener('click', () => {
+    subul.style.display = 'none'
+    angleup.style.display = 'none'
+    angledown.style.display = 'block'
 })
